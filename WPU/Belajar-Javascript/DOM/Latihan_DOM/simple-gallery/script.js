7aff59de821ff10761403a1c3ad9a9cee9a67d9d
Sugiharto <sugihartocg@gmail.com>
2023-04-27 22:33:49 +0700

    menyelesaikan fitur program

--- WPU/Belajar-Javascript/DOM/Latihan_DOM/simple-gallery/script.js
+++ WPU/Belajar-Javascript/DOM/Latihan_DOM/simple-gallery/script.js
@@ -1,12 +1,36 @@
 const container = document.querySelector('.container');
-
+const gallery = document.querySelector('.main-image');
 //Hapus
 function hapus(){
     
 };
 
 //ambil semua kelas
-const gambar = document.querySelectorAll('.thums');
+//thumb itu function
+const gambar = document.querySelectorAll('.thumbs');
 gambar.forEach(thumb => {
-    if（thumb.setAttribute = ）
+    thumb.addEventListener('click', function(){
+        switch (src) {
+            case "img/1.jpg":
+                gallery.setAttribute("src", "img/1.jpg");
+                break;
+            case "img/2.jpg":
+                gallery.setAttribute("src", "img/2.jpg");
+                break;
+            case "img/3.jpg":
+                gallery.setAttribute("src", "img/3.jpg");
+                break;
+            case "img/4.jpg":
+                gallery.setAttribute("src", "img/4.jpg");
+                break;
+            case "img/5.jpg":
+                gallery.setAttribute("src", "img/5.jpg");
+                break;
+            case "img/6.jpg":
+                gallery.setAttribute("src", "img/6.jpg");
+                break;
+            default:
+                break;
+        }
+    });
 });
