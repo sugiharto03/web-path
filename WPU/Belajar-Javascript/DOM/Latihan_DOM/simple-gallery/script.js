e61d1e7505eb066cd618af5e1f31ad5b1bf9819a
Sugiharto <sugihartocg@gmail.com>
2023-04-27 21:54:15 +0700

    melanjutkan mini-projek gallery image

---
+++ WPU/Belajar-Javascript/DOM/Latihan_DOM/simple-gallery/script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+//Hapus
+function hapus(){
+    
+};
+
+//ambil semua kelas
+const gambar = document.querySelectorAll('.thums');
+gambar.forEach(thumb => {
+    if（thumb.setAttribute = ）
+});
